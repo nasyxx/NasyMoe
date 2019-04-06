28406e4445539541a56b1788c7b56c0fd7ddd8c1
Nasy <nasyxx+git@gmail.com>
2019-04-06 19:00:53 +0800

    Finished basic layout style.

--- src/Style.hs
+++ src/Style.hs
@@ -28,7 +28,8 @@ There are more things in heaven and earth, Horatio, than are dreamt.
 --------------------------------------------------------------------------------
 
 -}
-
+{-# LANGUAGE OverloadedStrings,
+             OverloadedLists #-}
 --------------------------------------------------------------------------------
 -- |
 -- Filename   : Style.hs
@@ -43,18 +44,173 @@ There are more things in heaven and earth, Horatio, than are dreamt.
 --------------------------------------------------------------------------------
 module Main (main) where
 --------------------------------------------------------------------------------
-import           Clay                           ( Css
-                                                , putCss
-                                                , (|>)
-                                                , body
-                                                , (?)
-                                                , background
-                                                , black
+import           Prelude                 hiding ( rem
+                                                , (**)
+                                                , div
                                                 )
+--------------------------------------------------------------------------------
+import           Clay                    hiding ( fontColor )
+import qualified Clay.Flexbox                  as CF
+--------------------------------------------------------------------------------
+
 
 main :: IO ()
-main = putCss layout
+main = putCss $ basic >> layout >> blog
+
+
+basic :: Css
+basic = do
+    -- | Quote style
+    blockquote ? do
+        flip (sym2 margin) 0 $ rem 1
+        borderLeft solid (rem 1) quoteColor
+        p <? color (lighten 0.2 fontColor)
+
+    -- | Paragraph style
+    p ? do
+        sym2 padding 0 $ rem 1
+
+    -- | List Style
+    ul ? do
+        (borderLeft <> borderRight) solid (rem 0.5) ulColor
+        sym padding (px 1)
+    li ? do
+        listStyleType none
+        borderLeft solid (rem 0.5) liColor
+        (borderTop <> borderBottom) solid (px 1) transparent
+        flip (sym2 margin) 0 $ px 1
+        sym2 padding 0 $ px 2
+
+        hover & borderColor (darken 0.5 liColor)
+
+        a <? display block
+
+    -- | Hyperlink Style
+    a # "@href" ? do
+        color hyperColor
+        active & color hyperColorLight
+        hover & color hyperColor
+        visited & color hyperColorDark
+
+
+    -- | Pre and Soucecode
+    (pre <> ".sourceCode" <> code) ? fontFamily
+        ["Operator Mono SSm", "Operator Mono", "FiraCode", "hermit"]
+        [monospace]
+
+    -- | Everything Style
+    star ? transition "all" 0.4 easeInOut 0
+
 
 layout :: Css
-layout = body ? do
-    background black
+layout = do
+    body ? do
+        -- | Body Box
+        sym2 padding 0 $ rem 1
+        sym margin auto
+
+        -- | Color
+        backgroundColor bgColor
+        color fontColor
+
+        -- | Width & Height
+        minHeight $ vh 100
+        maxWidth $ px 840
+        minWidth $ px 320 @-@ rem 2
+
+        -- | Display
+        display flex
+        flexFlow column CF.nowrap
+        alignItems stretch
+
+        -- | Shadow
+        boxShadow
+            [ bsColor shadowColor $ shadowWithBlur nil nil (px 3)
+            , bsInset . bsColor shadowColor $ shadowWithBlur 0 0 (px 3)
+            ]
+        hover & boxShadow
+            [ bsColor shadowColor $ shadowWithBlur nil nil (px 5)
+            , bsInset . bsColor shadowColor $ shadowWithBlur 0 0 (px 5)
+            ]
+
+
+    "#header" ? do
+        display flex
+        alignItems center
+
+        ul ? do
+            borderRightWidth nil
+            marginLeft $ rem 1
+
+        noUnderline
+
+        h1 <? fontFamily ["Chalkduster"] [fantasy]
+
+        ".hide-in-header" ? display none
+
+        sym borderRadius $ px 5
+        borderBottom dashed (px 5) lineColor
+        boxShadow [bsColor lineColor2 $ shadowWithBlur nil (px 2) (px 3)]
+        hover & boxShadow
+            [bsColor lineColor2 $ shadowWithBlur nil (px 2) (px 5)]
+
+
+    "#footer" ? do
+        display flex
+        flexFlow column CF.nowrap
+        flexGrow 1
+        flexShrink 0
+        justifyContent spaceBetween
+
+        noUnderline
+        header <? do
+            display flex
+            marginBottom $ rem 1
+            sym borderRadius $ px 5
+
+            nav <? do
+                paddingRight $ rem 2
+                ul <? borderRightColor transparent
+
+            boxShadow [bsColor lineColor2 $ shadowWithBlur nil (px 2) (px 3)]
+            hover & boxShadow
+                [bsColor lineColor2 $ shadowWithBlur nil (px 2) (px 5)]
+
+        borderTop dashed (px 5) lineColor
+        sym borderRadius $ px 5
+
+    "#copyright" ? do
+        fontFamily ["American Typewriter"] [cursive, serif]
+        fontSizeCustom smaller
+        textAlign center
+        color "#d988bc"
+
+
+blog :: Css
+blog = do
+    section # ".blog-section" ? do
+        (div # ".sourceCode" <> pre) <? do
+            overflowX scroll
+            sym2 padding 0 (rem 1)
+
+        ul ? borderWidth nil
+        li # hover ? (borderTopColor <> borderBottomColor) transparent
+
+
+noUnderline :: Css
+noUnderline = a ? textDecoration none
+
+
+bgColor, fontColor, quoteColor, ulColor, liColor, hyperColor, hyperColorDark, hyperColorLight, shadowColor, lineColor, lineColor2
+    :: Color
+bgColor = "#ffeab6"
+fontColor = "#303a52"
+quoteColor = "#fc85ae"
+ulColor = "#9e579d"
+liColor = lighten 0.2 "#f69d9d"
+hyperColorDark = "#574b90"
+hyperColor = "#9e579d"
+hyperColorLight = "#fc85ae"
+lineColor = "#70a1d7"
+lineColor2 = "#d988bc"
+shadowColor = "#f67280"
